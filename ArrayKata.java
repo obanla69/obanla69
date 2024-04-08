7f472f842408f1f912b1d26b93e5b45c1a724f45
obanla69 <165768744+obanla69@users.noreply.github.com>
2024-04-08 03:00:19 +0100

    Add files via upload

---
+++ ArrayKata.java
@@ -0,0 +1,105 @@
+public class Arraykata{
+
+		public static integer maximum(int[]maximum){
+
+		double max = myList[0];
+		int index0fMax = 0;
+		for(int i = 1; i < myList.length; i++){
+		if(myList[i] > max){
+		max = myList[i];
+		indexOfMax = i;
+
+	}
+
+    }
+
+		public static void findMinMax(int[]arr){
+		 int min = arr[0];
+		 int max = arr[0];
+		  for(int ii = 0; ii <arr.length;ii++){
+		    if(arr[ii]<min){
+			min =arr[ii];
+			}
+			else if(arr[ii]>max){
+			 max=arr[ii];
+		}		
+
+
+	}	
+	
+		public static int sum(int[]number){
+		 int sum = 0;
+		  for(int n : numbers){
+		   sum += n;
+		
+	 }
+	 return
+      }
+				
+
+		public static Sum_EvenNum(String[]args){
+		 Scanner input = new Scanner(System.in);
+		  System.out.print("Enter the Aray Limit : ");
+		int 1 = input.nextInt();
+		int[] a = new int[1];
+		int sum = 0;
+		for(int i = 0;i<1;i++){
+		 System.out.printf("Element of a [%d] : " i);
+		  a[i] = input.nextInt();
+		}
+		for (int e:a){
+			if(e%2=0)
+			   sum = sum + e;
+		}
+		System.out.println("Sum of Even Array Elements : "+sum);
+	}
+
+}
+		
+
+
+
+
+		public static int sumOdds(int[] numbers){
+		 int sum = 0;
+		  for(int i = 0; i < number.length;i++){
+			if(numbers[i]% 2 != 0){
+			sum += numbers[i];
+		}		
+	}			
+}		return sum;
+
+
+
+
+		public static int SumofA(int sumA){
+		 int square = 1;
+		int sum = 0;
+		int n = 6;
+		int [] A = new int[n];
+		 for(int i = 0; i < n; i++){
+			square = A[i]*A[i];
+			sum +=square;
+		}
+		return sum;
+		
+	}
+	
+			
+		
+		
+
+		public static int SumofB(int sumA){
+		 int square = 1;
+		int sum = 0;
+		int n = 6;
+		int [] B = new int[n];
+		 for(int i = 0; i < n; i++){
+			square = B[i]*B[i];
+			sum +=square;
+		}
+		return sum;
+		
+	}
+
+
